126c30f9988480ac9e0af4688783cfdf9968046d
VanceGoblin <zvkubajak@gmail.com>
2024-10-02 11:16:21 -0500

    Created IThought & thought Schema (problems w/ createdAt & reactions)

---
+++ src/models/Thought.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document, ObjectId } from "mongoose";
+
+interface IThought extends Document {
+  thoughtText: string;
+  createdAt: Date;
+  username: string;
+  reactions: [];
+}
+
+const thoughtSchema = new Schema<IThought>({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // * get: (timestamp: Date) => timestamp.toLocaleString(),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: {
+    // *
+  }
+});
