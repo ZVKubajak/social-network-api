d11930e20016c5389768ce365d392f0afbc6c90a
VanceGoblin <zvkubajak@gmail.com>
2024-10-04 10:35:36 -0500

    Created Methods for userController.ts (minor bug with code paths)

---
+++ src/controllers/userController.ts
@@ -0,0 +1,68 @@
+import { User } from "../models/index";
+import { Request, Response } from "express";
+
+export const getUsers = async (_req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId }).select("-__v");
+
+    if (!user) {
+      res.status(404).json({ message: "No user with this ID." });
+    } else {
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const dbUserData = await User.create(req.body);
+    res.json(dbUserData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidatiors: true, new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "No user with this ID." });
+    }
+
+    res.json(user);
+    return;
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+    if (!user) {
+      return res.status(404).json({ message: "No user with this ID." });
+    }
+
+    res.json(user);
+    return;
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
